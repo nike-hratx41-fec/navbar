29cc4432121a3e7528010bdaafb8e3c27a76ada5
Matt DeLong <mdelong42@gmail.com>
2019-06-03 15:28:47 -0500

    deployThis

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -158,11 +158,10 @@ class Navbar extends Component {
   }
 
   handleRemove(e) {
-    alert(`${e.target.value}`, "this is the color");
     const newRemoval = this.state.cart;
     // const style = window.getComputedStyle(document.getElementsByClassName('cartContain')[0]);
     for(var x = 0; x < this.state.cart.length; x++){
-      if(this.state.cart[x].sku === e.target.id && this.state.cart[x].size === e.target.value && this.state.cart[x].color === e.target.value){
+      if(this.state.cart[x].sku === e.target.id && this.state.cart[x].size === e.target.value){
         newRemoval.splice(x, 1);
         this.setState({ cart: newRemoval });
         x = this.state.cart.length;
@@ -223,7 +222,7 @@ class Navbar extends Component {
                     <div id={listItem.sku}>{listItem.size} M</div>
                     <div id={listItem.sku}>${listItem.price}</div>
                   </div>
-                  <img className="redRemove" id={listItem.sku} value={`${listItem.size} + ${listItem.color}`} src="http://ec2-18-221-123-158.us-east-2.compute.amazonaws.com/b5b22e1fb368e51b84d0c1ffea51a17d.png" onClick={this.handleRemove}></img>
+                  <img className="redRemove" id={listItem.sku} value={listItem.size} src="http://ec2-18-221-123-158.us-east-2.compute.amazonaws.com/b5b22e1fb368e51b84d0c1ffea51a17d.png" onClick={this.handleRemove}></img>
                 </div>
               )
             }
